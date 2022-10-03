11fd85134b1df3d7cdbeb4958e645cbaf77a502e
KeviinKeurvels <k.keurvels@students.ephec-ti.be>
2022-10-03 22:23:28 +0200

    Rectification en fonction de l'US et des demandes de la PO et du SM

--- Web App Frontend/src/Pages/Home/Home.js
+++ Web App Frontend/src/Pages/Home/Home.js
@@ -6,27 +6,11 @@ import './Home.css';
 import imgChicken from '../../Assets/homeBackGround2.png';
 import imgCat from '../../Assets/homeBackGround3.png';
 
-function getDate(beginDate){
-    /*
-    Function that calculates how many animals are abandonned from the beginning of the project
-    PRE: date of the beginning of the project
-    POST: number of animals abandonned from the beginning of the project
-    */
-    const d1 = new Date(beginDate);
-    const d2 = new Date();
-    let diffMilli = d2.getTime() - d1.getTime();  
-    //get number of days between the beginning of the project and today (-1 to not count the day of the beginning)
-    let diffMonth = Math.round(diffMilli / (1000 * 60 * 60 * 24)) - 1;
-    return Math.round(diffMonth*(65000/12/30));
-
-}
-
 function Home() {
     return (
         <div>
             <div className='homeContent1 scrollDiv'>
                 <h1>Ensemble luttons contre les abandons</h1>
-                <p><span style={{fontSize:'400%'}}>{getDate('09/15/22')}</span><br/> animaux ont été abandonnés depuis le début de la création de ce site.</p>
             </div>
             <div className='homeContent2 scrollDiv'>
                 <Container>
