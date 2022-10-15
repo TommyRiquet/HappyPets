6d5835560ca7215803650d1f499c427a43bbfbb6
Lopidurs <nathan.sancke@hotmail.fr>
2022-10-15 20:30:01 +0200

    Envoie des données de login vers le backend

--- Web App Frontend/src/Pages/Login/Login.js
+++ Web App Frontend/src/Pages/Login/Login.js
@@ -1,10 +1,31 @@
 /*Importing Components */
 import {Row, Col, Form, Button, Container } from 'react-bootstrap';
+import axios from "axios";
+import {useNavigate} from 'react-router-dom';
 /*Importing Styles*/
 import './Login.css';
 
 function Login() {
+    let navigate = useNavigate();
 
+    function sendForm(event) {
+        const user = {
+            Email: event.target[0].value,
+            Password: event.target[1].value
+        };
+
+        event.preventDefault()
+
+        axios.post("http://localhost/users/login", user)
+            .then(res => {
+                if (res.data.error) {
+                    alert(res.data.error)
+                } else {
+                    navigate('/')
+                    window.location.reload()
+                }
+            })
+    }
 
     return (
         <div className="Login">
@@ -12,7 +33,7 @@ function Login() {
                 <h2>Connexion</h2>
                 </Container>
                 <Container className='main-container'>
-                    <Form>
+                    <Form onSubmit={sendForm}>
                         <Row className="justify-content-md-center mb-4">
                             <Col xs lg="6">
                                 <Form.Label style={{float: "left"}} htmlFor="email">Adresse e-mail</Form.Label>
@@ -35,7 +56,7 @@ function Login() {
                     <Row className="justify-content-md-center">
                         <Col md="auto">
                             {/* We have to create the route Registration to make it work*/}
-                            <p>Vous n'avez pas encore de compte ? <a href='/registration' className='linkRegistration'>Cliquez ici</a> pour vous en créer un !</p>
+                            <p>Vous n'avez pas encore de compte ? <a href='/inscription' className='linkRegistration'>Cliquez ici</a> pour vous en créer un !</p>
                         </Col>
                     </Row>      
                 </Container>
