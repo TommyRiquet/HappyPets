cf9516c1c17dc0a3534055a839f362600fe8ca75
Marina <m.quadu@students.ephec.be>
2022-10-20 17:42:39 +0200

    ajout texte HomePage

--- Web App Frontend/src/Pages/Home/Home.js
+++ Web App Frontend/src/Pages/Home/Home.js
@@ -16,10 +16,12 @@ function Home() {
                 <Container>
                     <Row xs={1} md={2}>
                         <Col style={{marginTop: '10%'}}>
-                            <p>Près de <span style={{color:'orange'}}>65 000</span> animaux domestiques sont abandonnés tous les ans par leurs maîtres à l'approche de l'été.</p>
+                            <p>Près de <span style={{color:'orange'}}>65 000</span> animaux domestiques sont abandonnés 
+                            tous les ans. Si la majorité se produit à l'approche de l'été, d'autres sont une conséquence 
+                            des aléas de la vie.</p>
                         </Col>
                         <Col>
-                            <img id="imgChicken" src={imgChicken} alt="Picture of a chicken"/>
+                            <img id="imgChicken" src={imgChicken} alt="chicken"/>
                         </Col>
                     </Row>
                 </Container>
@@ -28,10 +30,12 @@ function Home() {
                 <Container>
                     <Row xs={1} md={2}>
                         <Col style={{marginTop: '8%', maxWeight: '100%', maxHeight: '100%'}}>
-                            <img id="imgCat" src={imgCat} alt="Picture of a cat"/>
+                            <img id="imgCat" src={imgCat} alt="cat"/>
                         </Col>
                         <Col style={{marginTop: '8%', maxWeight: '100%', maxHeight: '100%'}}>
-                            <p>Puppy kitty ipsum dolor sit good dog foot stick canary. Teeth Mittens grooming vaccine walk swimming nest good boy furry tongue heel furry treats fish. Cage run fast kitten dinnertime ball run foot park fleas throw house train licks stick dinnertime window.</p>
+                            <p>Hospitalisation d’urgence, perte d’un logement...sont autant de difficultés qui ne laissent parfois 
+                            d’autres choix que d’abandonner son animal. Durant les inondations de 2021, un énorme réseau d’entraide s’est
+                            créé via les réseaux sociaux.<br/> Nous continuons l’effort ici.</p>
                         </Col>
                     </Row>
                 </Container>
