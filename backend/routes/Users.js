8e0ace499f546346b4f40ac636d8b731e5f28065
Kevin Keurvels <k.keurvels@students.ephec.be>
2022-10-20 17:08:05 +0200

    Bug fix des forces
    Reparations dues aux nombreuxpush forces
    le package json a ete mis a jour pour mettre les nouveaux install
    les routes ont aussi ete mis a jour

--- backend/routes/Users.js
+++ backend/routes/Users.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const {Users} = require("../models")
+const {Users, Pets} = require("../models")
 const bcrypt = require("bcryptjs")
 const jwt = require('jsonwebtoken')
 const {myError} = require("../middleware/Error")
