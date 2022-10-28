3140106cb54453741cd85295bd3166b7a9fe9baf
Marina <m.quadu@students.ephec.be>
2022-10-28 14:21:13 +0200

    Un joli message pour Monsieur Castiaux
    Réimplémentation de la navbar dans les pages error et register parce que
    ça a disparu et on ne sait pas pourquoi...

--- Web App Frontend/src/Pages/Error/Error.js
+++ Web App Frontend/src/Pages/Error/Error.js
@@ -4,7 +4,8 @@ import CustomNavbar from "../../Components/CustomNavbar/CustomNavbar";
 function Error() {
     return (
         <div className="Error">
-            <CustomNavbar/>
+            <CustomNavbar color="rgba(47, 72, 88, 1)"/>
+            <h1>Oups, il n'y a rien ici</h1>
             <h1>Oups, il n'y a rien ici</h1>
         </div>
     );
