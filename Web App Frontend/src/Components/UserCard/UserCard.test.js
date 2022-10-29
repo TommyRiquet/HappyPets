70ba76bfd804ee917bcbab39df266f06c8ad0cf7
TommyRiquet <tommy.riquet@hotmail.com>
2022-10-29 19:35:47 +0200

    Fix test UserCard
    Suppression des tests pour l'age et le code postal car il ne sont plus
    utilisé dans le composant

--- Web App Frontend/src/Components/UserCard/UserCard.test.js
+++ Web App Frontend/src/Components/UserCard/UserCard.test.js
@@ -21,15 +21,11 @@ describe('Testing component props <Usercard />', () => {
             <UserCard proposition={FakeUser}/>
         )
         const propsFirstName = screen.getByTestId("proposition-name")
-        const propsAge = screen.getByTestId("proposition-age")
         const propsVille = screen.getByTestId("proposition-lieu")
-        const propsPostal = screen.getByTestId("proposition-postal")
 
 
         expect(propsFirstName.textContent).toBe(FakeUser.User.FirstName);
-        expect(propsAge.textContent).toBe(","+FakeUser.User.Age);
         expect(propsVille.textContent).toBe(FakeUser.User.City);
-        expect(propsPostal.textContent).toBe(","+FakeUser.User.Postal);
     })
 
     it('Should replace the FirstName with "Inconnu"', async () => {
@@ -43,17 +39,6 @@ describe('Testing component props <Usercard />', () => {
           expect(propsFirstName.textContent).toBe("Inconnu");
       })
 
-    it('Should not display the Age if its negative', async () => {
-        const FakeUser = {User : {FirstName: "Kevin", Age: -21, City: 'Wavre', Postal: 1330}}
-
-        
-        render(
-              <UserCard proposition={FakeUser}/>
-          )
-          const propsAge = screen.getByTestId("proposition-age")
-          expect(propsAge.textContent).toBe("");
-    })
-
     it('should replace the place with "Inconnu"', async () =>{
 
         const FakeUser = {User : {FirstName: "Kevin", Age: 21, City: '', Postal: 1330}}
@@ -65,16 +50,4 @@ describe('Testing component props <Usercard />', () => {
         expect(propsVille.textContent).toBe("Inconnu");
     })
 
-    it('should not display the Postal if its different of 4 number', async () => {
-        const FakeUser = {User : {FirstName: "Kevin", Age: 21, City: 'Wavre', Postal: 330}}
-
-
-        render(
-            <UserCard proposition={FakeUser}/>
-        )
-
-        const propsPostal = screen.getByTestId("proposition-postal")
-        expect(propsPostal.textContent).toBe("");
-
-    })
 })
