b6e6d2bd4cc47209136aa2aea08e4de979ba32c1
TommyRiquet <tommy.riquet@hotmail.com>
2022-11-04 00:33:15 +0100

    US#6 + US#93
    Je m'excuse d'avance pour ce commit qui est beaucoup trop gros , j'aurai
    pu diviser celui-ci en plusieurs plus petits commits dinstincts, mais au
    moins, on ne pourra pas dire que je gonfle mon nombre de commit *wink
    wink*
    Bref, j'ai ajouté le filtre sur la page d'annonces, en suivant la US#6
    que je vous invite à lire car elle est très intérréssante je trouve.
    J'ai aussi corrigé le bug de la barre de navigation qui décalait
    l'intégralité du site vers le bas (oui, je sais que j'aurai pu le commit
    à part)
    Et j'ai aussi implémenté les icons correspondant aux compatibilités des
    animaux dans animalCard, comme respectant la maquette

--- backend/routes/Annonces.js
+++ backend/routes/Annonces.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const {Op} = require('sequelize')
 const router = express.Router()
 const {Annonces,Pets,Users,PetsAnnonces} = require("../models")
 
@@ -7,14 +8,43 @@ router.get("/", async (req, res) => {
     /*
     *   Route initial qui retourne les 20 premières annonces
     */
-    if(req.query.offset===undefined || req.query.offset === "0"){
+    let offset = req.query.offset || 0
+    let limit = req.query.limit || 20
+    let typePet = req.query.typePet || ["Chien","Chat","Rongeur","Oiseau","Poisson","NAC"]
+    let typeAnnonce = req.query.typeAnnonce || ["Promenade","Logement","Garde à domicile","Soins à domicile"]
+    let isDogFriendly = req.query.DogFriendly==="true" || [true,false]
+    let isCatFriendly = req.query.CatFriendly==="true" || [true,false]
+    let isKidFriendly = req.query.KidFriendly==="true" || [true,false]
+
+
             const listOfAnnonces = await Annonces.findAll(
                 {
-                    limit: 20,
+                    limit: parseInt(limit),
+                    offset: parseInt(offset),
+                    where: { // Where type of annonce is in the list of type of annonce
+                        Type: {
+                            [Op.or]: [typeAnnonce]
+                        }
+                    },
                     attributes: ['Type','DateBegin','DateEnd'],
                         include: [ {
                             model:Pets,
-                            attributes: ['Name','Type','Race','Age','Sexe','Sterile','Weight','Height'],
+                            attributes: ['Name','Type','Race','Age','Sexe','Sterile','Weight','Height','DogFriendly','CatFriendly','KidFriendly'],
+                            where : { //Where type of pet is in the list of typePet and isDogFriendly, isCatFriendly and isKidFriendly are equal to the value in the request
+                                [Op.and]: [
+                                {Type :{
+                                        [Op.or]:[typePet]
+                                    }
+                                },
+                                {
+                                    [Op.and]: [
+                                        {DogFriendly: isDogFriendly},
+                                        {CatFriendly: isCatFriendly},
+                                        {KidFriendly: isKidFriendly}
+                                    ]
+                                }
+                                ]             
+                                },
                             include: [ {
                                 model:Users,
                                 attributes: ['Firstname','City'],
@@ -22,29 +52,7 @@ router.get("/", async (req, res) => {
                         }]
                 }
             )
-            res.json(listOfAnnonces)
-        
-    }else{
-        /*
-        *   Route secondaire qui retourne les 6 annonces suivantes
-        */
-        const listOfAnnonces = await Annonces.findAll(
-            {
-                limit: 6,
-                offset: parseInt(req.query.offset),
-                attributes: ['Type','DateBegin','DateEnd'],
-                    include: [ {
-                        model:Pets,
-                        attributes: ['Name','Type','Race','Age','Sexe','Sterile','Weight','Height'],
-                        include: [ {
-                            model:Users,
-                            attributes: ['Firstname','City'],
-                        }]
-                    }]
-            }
-        )
         res.json(listOfAnnonces)
-    }
 
   
 })
