f089c97678b552af810f8f2181ff18dc7336afbb
TommyRiquet <tommy.riquet@hotmail.com>
2022-11-09 21:48:19 +0100

    US#97
    Ajout de la page 404 en cas d'erreur dans l'url
    Ajout d'un bouton pour rediriger les utilisateurs vers la page d'accueil
    lorsqu'il se retrouve sur le page.

--- Web App Frontend/src/Pages/Error/Error.js
+++ Web App Frontend/src/Pages/Error/Error.js
@@ -1,14 +1,34 @@
-import './Error.css';
+/*Importing Components*/
 import CustomNavbar from "../../Components/CustomNavbar/CustomNavbar";
+import { Button, Container } from "react-bootstrap";
+
+/*Importing Images*/
+import sadDog from "../../Assets/sad-dog.png";
+
+/*Importing Style*/
+import "./Error.css";
 
 function Error() {
-    return (
-        <div className="Error">
-            <CustomNavbar color="rgba(47, 72, 88, 1)"/>
-            <h1>Oups, il n'y a rien ici</h1>
-            <h1>Oups, il n'y a rien ici</h1>
-        </div>
-    );
+  return (
+    <div className="Error">
+      <CustomNavbar
+        textLinkOne="Propositions"
+        linkOne="/propositions"
+        textLinkTwo="Annonces"
+        linkTwo="/annonces"
+        color="rgba(47, 72, 88, 1)"
+      />
+      <Container>
+        <h1 className="title">Perdu ?</h1>
+        <h4 className="under-title">Page 404</h4>
+        <Button className="home-button" href='/'>Accueil</Button>
+      </Container>
+
+      <div className="image-container">
+        <img src={sadDog} className="dog-picture" alt="Chien Triste" />
+      </div>
+    </div>
+  );
 }
 
 export default Error;
