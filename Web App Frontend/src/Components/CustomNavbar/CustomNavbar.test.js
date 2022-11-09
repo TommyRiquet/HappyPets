450f61ba5becf743ced3f641a98ef2bb86668e2d
Marina <m.quadu@students.ephec.be>
2022-11-09 17:28:42 +0100

    testing navbar
    ajout tests unitaires de la custom navbar

--- Web App Frontend/src/Components/CustomNavbar/CustomNavbar.test.js
+++ Web App Frontend/src/Components/CustomNavbar/CustomNavbar.test.js
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, screen} from '@testing-library/react';
 import CustomNavbar from './CustomNavbar';
 
 describe('CustomNavbar', () => {
@@ -6,5 +6,70 @@ describe('CustomNavbar', () => {
         render(
             <CustomNavbar/>
         )
+    })
+})
+
+
+
+describe('Props Test for the component CustomNavbar', () => {
+    it('Should display the Link One correctly', async () => {
+        render(
+            <CustomNavbar
+                textLinkOne="TextLinkOne"
+                linkOne="/linkone"
+                textLinkTwo="TextLinkTwo"
+                linkTwo="/linktwo"
+                color="rgba(47, 72, 88, 1)"
+            />
+        )
+
+        const propsLinkOne = screen.getByTestId("link-one")
+        expect(propsLinkOne.textContent).toBe("TextLinkOne");
+
+    })
+    it('Should display the Link Two correctly', async () => {
+        render(
+            <CustomNavbar
+                textLinkOne="TextLinkOne"
+                linkOne="/linkone"
+                textLinkTwo="TextLinkTwo"
+                linkTwo="/linktwo"
+                color="rgba(47, 72, 88, 1)"
+            />
+        )
+
+        const propsLinkTwo = screen.getByTestId("link-two")
+        expect(propsLinkTwo.textContent).toBe("TextLinkTwo");
+
+    })
+    it('Should have the correct href for LinkOne', async () => {
+        render(
+            <CustomNavbar
+                textLinkOne="TextLinkOne"
+                linkOne="/linkone"
+                textLinkTwo="TextLinkTwo"
+                linkTwo="/linktwo"
+                color="rgba(47, 72, 88, 1)"
+            />
+        )
+
+        const propsLinkOne = screen.getByTestId("link-one")
+        expect(propsLinkOne).toHaveAttribute('href','/linkone');
+
+    })
+    it('Should have the correct href for LinkTwo', async () => {
+        render(
+            <CustomNavbar
+                textLinkOne="TextLinkOne"
+                linkOne="/linkone"
+                textLinkTwo="TextLinkTwo"
+                linkTwo="/linktwo"
+                color="rgba(47, 72, 88, 1)"
+            />
+        )
+
+        const propsLinkTwo = screen.getByTestId("link-two")
+        expect(propsLinkTwo).toHaveAttribute('href','/linktwo');
+
     })
 })
