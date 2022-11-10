0b79d2a8c89a22cc6cd1370b66a8bb8afbc96813
Kevin Keurvels <k.keurvels@students.ephec.be>
2022-11-10 10:21:54 +0100

    Ajout un test integration pour la page Account
    Ajout dans la page Account.test.js du test d'integration disant qu'il y
    aura qu'un appel a la db et que ca va bien faire la reuqte note

--- Web App Frontend/src/Pages/Account/Account.test.js
+++ Web App Frontend/src/Pages/Account/Account.test.js
@@ -1,5 +1,8 @@
-import { render } from '@testing-library/react';
+import {  render } from '@testing-library/react';
+import React from 'react';
+import { act } from 'react-dom/test-utils';
 import Account from './Account';
+import {BrowserRouter} from 'react-router-dom';
 
 describe('Account', () => {
     it('Should render without crash', async () => {
@@ -7,4 +10,26 @@ describe('Account', () => {
             <Account/>
         )
     })
+})
+
+
+describe('Integration Tests for the <Account> Page', () => { 
+
+    it('Should fetch the /account route once with the correct url', async () => {
+        const jestSpy = jest.spyOn(global, 'fetch')
+        
+        render(
+            <BrowserRouter>
+                <Account/>
+            </BrowserRouter>        );
+        
+        await act(async () => {
+            expect(jestSpy).toHaveBeenCalledTimes(1);
+            expect(jestSpy).toHaveBeenCalledWith("http://localhost:3001/users/info?id=3");
+        })
+
+
+        }
+    )
+
 })
