9774b6bb69475cc7d3e612d178434ed7ebc8ea49
Marina <m.quadu@students.ephec.be>
2022-11-14 18:09:46 +0100

    us #42
    test #42 et css

--- Web App Frontend/src/Pages/DetailAnnonce/DetailAnnonce.test.js
+++ Web App Frontend/src/Pages/DetailAnnonce/DetailAnnonce.test.js
@@ -1,10 +1,44 @@
-import { render } from '@testing-library/react';
+import {  render, screen } from '@testing-library/react';
+import React from 'react';
+import { act } from 'react-dom/test-utils';
 import DetailAnnonce from './DetailAnnonce';
+import {BrowserRouter} from 'react-router-dom';
 
-describe('DetailAnnonce', () => {
+// Path: Web App Frontend\src\Pages\DetailAnnonce\DetailAnnonce.js
+
+  
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+describe('Render Tests for the <Annonces> Page', () => {
     it('Should render without crash', async () => {
         render(
-            <DetailAnnonce/>
+            <BrowserRouter>
+                <DetailAnnonce/>
+            </BrowserRouter>
         )
     })
+})
+
+
+describe('Integration Tests for the <DetailAnnonce> Page', () => { 
+
+    it('Should fetch the /DetailAnnonce route once with the correct url', async () => {
+        const jestSpy = jest.spyOn(global, 'fetch')
+        
+        render(
+            <BrowserRouter>
+                <DetailAnnonce/>
+            </BrowserRouter>        );
+        
+        await act(async () => {
+            expect(jestSpy).toHaveBeenCalledTimes(1);
+            expect(jestSpy).toHaveBeenCalledWith("http://localhost:3001/annonces/detailAnnonce?id=undefined");
+        })
+
+
+        }
+    )
+
 })
