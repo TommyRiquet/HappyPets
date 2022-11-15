039db7a5caa48033e92a0dd3cf217e0658207e3a
TommyRiquet <tommy.riquet@hotmail.com>
2022-11-15 11:33:25 +0100

    US#12
    Fin de la US#12
    Veuillez lire celle-ci avant de review (je sais c'est chiant mais il le faut)
    Voila voila , si question => Google
    Bonne chance ^^

--- Web App Frontend/src/Pages/DetailAnnonce/DetailAnnonce.test.js
+++ Web App Frontend/src/Pages/DetailAnnonce/DetailAnnonce.test.js
@@ -1,8 +1,71 @@
 import {  render, screen } from '@testing-library/react';
 import React from 'react';
 import { act } from 'react-dom/test-utils';
-import DetailAnnonce from './DetailAnnonce';
 import {BrowserRouter} from 'react-router-dom';
+import {DetailAnnonce, AddPetModal} from './DetailAnnonce';
+
+const annonce = {
+    "id": 8,
+    "Comment": "Commentaire de l'annonce de Chausette et Jean-David",
+    "Type": "Promenade",
+    "DateBegin": "2022-10-13T00:00:00.000Z",
+    "DateEnd": "2022-10-13T00:00:00.000Z",
+    "Pets": [
+        {
+            "id": 2,
+            "Name": "Chaussette",
+            "Type": "Chat",
+            "Race": "Exotic",
+            "Age": "1",
+            "Sexe": "F",
+            "Weight": 5,
+            "Height": "Normal",
+            "Comment": "Meilleur chat ever",
+            "DogFriendly": true,
+            "CatFriendly": true,
+            "KidFriendly": true,
+            "User": {
+                "id": 2,
+                "Firstname": "Tommy",
+                "City": "Perwez"
+            },
+            "PetsAnnonces": {
+                "createdAt": "2022-11-14T22:10:48.000Z",
+                "updatedAt": "2022-11-14T22:10:48.000Z",
+                "AnnonceId": 8,
+                "PetId": 2
+            }
+        },
+        {
+            "id": 12,
+            "Name": "Jean-Louis-David",
+            "Type": "Oiseau",
+            "Race": "Toucan",
+            "Age": "2",
+            "Sexe": "M",
+            "Weight": 2,
+            "Height": "Normal",
+            "Comment": "Best Toucan ever",
+            "DogFriendly": true,
+            "CatFriendly": false,
+            "KidFriendly": true,
+            "User": {
+                "id": 2,
+                "Firstname": "Tommy",
+                "City": "Perwez"
+            },
+            "PetsAnnonces": {
+                "createdAt": "2022-11-14T22:10:48.000Z",
+                "updatedAt": "2022-11-14T22:10:48.000Z",
+                "AnnonceId": 8,
+                "PetId": 12
+            }
+        }
+    ]
+}
+
+
+
 
 // Path: Web App Frontend\src\Pages\DetailAnnonce\DetailAnnonce.js
 
@@ -13,6 +76,7 @@ import {BrowserRouter} from 'react-router-dom';
 
 describe('Render Tests for the <Annonces> Page', () => {
     it('Should render without crash', async () => {
+        localStorage.setItem('user', JSON.stringify({id:1}));
         render(
             <BrowserRouter>
                 <DetailAnnonce/>
@@ -33,7 +97,7 @@ describe('Integration Tests for the <DetailAnnonce> Page', () => {
             </BrowserRouter>        );
         
         await act(async () => {
-            expect(jestSpy).toHaveBeenCalledTimes(1);
+            expect(jestSpy).toHaveBeenCalledTimes(2);
             expect(jestSpy).toHaveBeenCalledWith("http://localhost:3001/annonces/detailAnnonce?id=undefined");
         })
 
@@ -42,3 +106,52 @@ describe('Integration Tests for the <DetailAnnonce> Page', () => {
     )
 
 })
+
+
+
+
+describe('Render Test for the component AddPetModal', () => {
+    it('Should render without crash', async () => {
+        render(
+            <>
+                <AddPetModal pets={[]}/>
+            </>
+        )
+    })
+})
+
+
+
+describe('Props Test for the component AddPetModal', () => {
+    it('Should display the error message when no pets is given', async () => {
+        render(
+            <AddPetModal pets={[]} show={true}/>
+        )
+        const ListPets = screen.getByTestId("list-pets");
+        expect(ListPets.textContent).toBe("Aucun animal :/")
+    })
+
+    it('Should display a list of pets when some pets are given', async () => {
+        render(
+            <AddPetModal pets={[[
+                {
+                    "id": 2,
+                    "Name": "Chaussette",
+                    "Type": "Chat",
+                },
+                {
+                    "id": 10,
+                    "Name": "Jean-Louis",
+                    "Type": "NAC",
+                }
+            ]]} 
+            annonce={annonce}
+            show={true}/>
+        )
+        const ListPets = screen.getByTestId("list-pets").innerHTML;
+        expect(ListPets).toBe("<div class=\"pet-image-modal-col col\"><img alt=\"\" class=\"pet-image-modal\"><span></span></div>")
+    })
+
+
+})
+
