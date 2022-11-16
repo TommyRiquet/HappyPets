57daceedeb59a304d405ea3edc1005c8abc14330
KeviinKeurvels <k.keurvels@students.ephec-ti.be>
2022-11-16 19:12:32 +0100

    Avancement dans le frontend

--- Web App Frontend/src/Pages/Account/Account.test.js
+++ Web App Frontend/src/Pages/Account/Account.test.js
@@ -1,11 +1,8 @@
 import {  render } from '@testing-library/react';
 import React from 'react';
-import { act } from 'react-dom/test-utils';
 import Account from './Account';
-import {BrowserRouter} from 'react-router-dom';
 
 /*Importing Config*/
-import config from "../../config.json";
 
 describe('Account', () => {
     it('Should render without crash', async () => {
