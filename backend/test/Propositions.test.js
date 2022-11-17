5b9513e58d163a83732a4c8f57dacdc5f2703b83
quentinrld <71373028+quentinrld@users.noreply.github.com>
2022-11-17 14:48:33 +0100

    US#69
    ajout dans gitignore du fichier coverage
    ajout aux models /Annonces et /Propositions la liaison via /PropAnnonces
    optimisation de la route /propositions affin de pouvoir la tester
    test de la route proposition
    
    modification du CSS pour la page detailAnnonce
    Modification du composant UserCard afin d'y afficher ce que les personnes propose, leur différents animaux...
    test des différents éléments de la UserCard
    ajout de CSS des différent élément ajouté
    Modification de la URL de la page /proposition

--- backend/test/Propositions.test.js
+++ backend/test/Propositions.test.js
@@ -11,3 +11,142 @@ describe('Testing for the /propositions routes', function() {
   
   
   });
+
+describe('Testing the querystring of /proposition', function(){
+    it('should get 10 data', async() =>{
+        const res = await request(app)
+            .get('/propositions')
+        expect(res.body.length).toBe(10);
+    })
+    it('should get 6 data', async() =>{
+        const res = await request(app)
+            .get('/propositions?limit=6')
+        expect(res.body.length).toBe(6);
+    })
+    it('should return empty when array is not numeric', async() =>{
+        const res = await request(app)
+            .get('/propositions?limit=x')
+        expect(res.body.length).toBe(0);
+    })
+    it('should return empty when array is not negativ', async() =>{
+        const res = await request(app)
+            .get('/propositions?limit=-2')
+        expect(res.body.length).toBe(0);
+    })
+})
+
+describe('Testing for the typeAnimalProposition queryString of /propositions', function() {
+
+    it('Should only get Dogs', async() =>{
+        const res = await request(app)
+            .get('/propositions?typeAnimalProposition=Chien')
+        
+        for(i in res.body){
+            for (pets in res.body[i].Pets){
+                expect(res.body[i].Pets[pets].Type).toBe("Chien");
+            }
+        }
+    }) 
+
+    it('Should only get Cats', async() =>{
+        const res = await request(app)
+            .get('/propositions?typeAnimalProposition=Chats')
+        
+        for(i in res.body){
+            for (pets in res.body[i].Pets){
+                expect(res.body[i].Pets[0].Type).toBe("Chats");
+            }
+        }
+    }) 
+
+    it('Should only get Rodents', async() =>{
+        const res = await request(app)
+            .get('/propositions?typeAnimalProposition=Rongeur')
+        
+        for(i in res.body){
+            for (pets in res.body[i].Pets){
+                expect(res.body[i].Pets[0].Type).toBe("Rongeur");
+            }
+        }
+    }) 
+
+    it('Should only get Birds', async() =>{
+        const res = await request(app)
+            .get('/propositions?typeAnimalProposition=Oiseau')
+        
+        for(i in res.body){
+            for (pets in res.body[i].Pets){
+                expect(res.body[i].Pets[0].Type).toBe("Oiseau");
+            }
+        }
+    }) 
+
+    it('Should only get Fishes', async() =>{
+        const res = await request(app)
+            .get('/propositions?typeAnimalProposition=Poisson')
+        
+        for(i in res.body){
+            for (pets in res.body[i].Pets){
+                expect(res.body[i].Pets[0].Type).toBe("Poisson");
+            }
+        }
+    }) 
+
+    it('Should only get NAC', async() =>{
+        const res = await request(app)
+            .get('/propositions?typeAnimalProposition=NAC')
+        
+        for(i in res.body){
+            for (pets in res.body[i].Pets){
+                expect(res.body[i].Pets[0].Type).toBe("NAC");
+            }
+        }
+    }) 
+
+    it('Should find everything', async() =>{
+        const res = await request(app)
+            .get('/propositions')
+        
+        for(i in res.body){
+            for (pets in res.body[i].Pets){
+                expect(res.body[i].Pets[0].Type).toMatch(/Chien|Chat|Rongeur|Oiseau|Poisson|NAC/);
+            }
+        }
+    }) 
+    
+  });
+
+  describe('Testing for the typeProposition queryString of /propositions', function() {
+
+    it('Should only get Walks', async() =>{
+        const res = await request(app)
+            .get('/propositions?typeProposition=Promenade')
+
+            expect(res.body[0].Type).toBe("Promenade");
+
+    }) 
+
+    it('Should only get housing', async() =>{
+        const res = await request(app)
+            .get('/propositions?typeProposition=Logement')
+
+            expect(res.body[1].Type).toBe("Logement");
+    }) 
+
+    it('Should only get Home Care', async() =>{
+        const res = await request(app)
+            .get('/propositions?typeProposition=Soins%20à%20domicile')
+
+            expect(res.body[2].Type).toBe("Soins à domicile");
+
+    }) 
+
+    it('Should only get Home Care(keep)', async() =>{
+        const res = await request(app)
+            .get('/propositions?typeProposition=Garde%20à%20domicile')
+
+            expect(res.body[5].Type).toBe("Garde à domicile");
+
+    }) 
+  
+  });
