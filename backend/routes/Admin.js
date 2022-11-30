317a8a4f6329dc4285f2724ce73a674c3797c945
raphaelmarto <r.marto@students.ephec.be>
2022-11-30 17:42:05 +0100

    route raw Remplacement de la route pour ne faire que 1 requet a la place de 2 Abandon de Sequalize pour une requete raw car manque de connaissance en sequalize

--- backend/routes/Admin.js
+++ backend/routes/Admin.js
@@ -1,18 +1,19 @@
 const express = require('express')
+const { QueryTypes } = require('sequelize');
 const router = express.Router()
-const {Admin,Users} = require("../models")
+const {Admin,Users, sequelize} = require("../models")
 
 router.get("/", async (req, res) => {
     let offset = req.query.offset || 0
     let limit = req.query.limit ? ((req.query.limit>0 && !isNaN(req.query.limit)) ? req.query.limit : 0): 20
 
-    const allreport = await Admin.findAll({
-        limit:parseInt(limit),
-        offset:parseInt(offset),
-        include : [{
-            model:Users,
-            attributes:['id','FirstName'],}]
-    })
+    const allreport = await sequelize.query(
+        "SELECT A.*, US.FirstName 'NomSus', U.FirstName 'NomUser'\
+        FROM `Admins` A \
+        JOIN `Users` US ON US.id = A.SuspectId \
+        JOIN `Users` U ON U.id = A.UserId \
+        LIMIT "+limit+" OFFSET "+offset
+    , { type: QueryTypes.SELECT });
     res.json(allreport)
 })
 
@@ -25,7 +26,7 @@ router.get("/findtype", async (req, res) => {
         where: {Type: req.query.type},
         include : [{
         model:Users,
-        attributes:['id','FirstName'],}]
+        attributes:['id','FirstName']}]
 })
     res.json(allreport)
 })
