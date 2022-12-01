586b0fe0c1d89641f5e4e973157632380ef7a85a
KeviinKeurvels <k.keurvels@students.ephec-ti.be>
2022-12-01 22:52:24 +0100

    Photo barre de nav
    Ajout d'une photo dans la barre de navigation si l'utilisateur est
    connecté
    
    Essai dans Home.js pour donner le nom de la photo dans le local storage
    à la navbar
    
    Pour le moment, ça fonctionne pas mais on a de l'espoir

--- Web App Frontend/src/Components/CustomNavbar/CustomNavbar.js
+++ Web App Frontend/src/Components/CustomNavbar/CustomNavbar.js
@@ -1,8 +1,14 @@
 import './CustomNavbar.css';
-import {Navbar, Container} from "react-bootstrap";
-import Nav from 'react-bootstrap/Nav';  
-import PerroquetImage from '../../Assets/bird.png'; 
+import { Navbar, Container } from "react-bootstrap";
+import Nav from 'react-bootstrap/Nav';
 import { useEffect } from 'react';
+
+/*Importing Images*/
+import ProfilePicDefault from '../../Assets/profilePictureDefault.png'
+import PerroquetImage from '../../Assets/bird.png';
+
+/*Importing Config*/
+import config from "../../config.json";
 
 function CustomNavbar(props) {
   useEffect(() => {
@@ -15,28 +21,30 @@ function CustomNavbar(props) {
       <Container fluid>
         <Navbar.Brand href="/" className='title-navbar'>
           <img
-              src={PerroquetImage}
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-              alt="Logo de QTPets"
-            />
-            QTPets 
-            </Navbar.Brand>
+            src={PerroquetImage}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+            alt="Logo de QTPets"
+          />
+          QTPets
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav className="me-auto"></Nav>
           <Nav className="d-flex">
+            <Nav.Link href={props.linkOne} data-testid='link-one'>{props.textLinkOne}</Nav.Link>
+            <Nav.Link href={props.linkTwo} data-testid='link-two'>{props.textLinkTwo}</Nav.Link>
+            {!localStorage.getItem('user') ?
+              <>
+                <Nav.Link href={props.linkThree} data-testid='link-three'>{props.textLinkThree}</Nav.Link>
+                <Nav.Link href={props.linkFour} data-testid='link-four'>{props.textLinkFour}</Nav.Link>
+              </>
+              :           
+              <img className='image-profil' src={config.API_URL + "/images/" + props.PhotoLink} alt="Accès au profil" />
 
-          <Nav.Link className='linkcss' href={props.linkOne} data-testid='link-one'>{props.textLinkOne}</Nav.Link>
-          <Nav.Link  className='linkcss' href={props.linkTwo} data-testid='link-two'>{props.textLinkTwo}</Nav.Link>
-            {!localStorage.getItem('user') ? 
-            <>
-              <Nav.Link href={props.linkThree} data-testid='link-three'>{props.textLinkThree}</Nav.Link>
-              <Nav.Link href={props.linkFour} data-testid='link-four'>{props.textLinkFour}</Nav.Link>
-            </>
-            : null
             }
+
           </Nav>
         </Navbar.Collapse>
       </Container>
