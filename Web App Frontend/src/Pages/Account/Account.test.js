74ca934c911d9e24639d236e45b74a466db961bc
Lopidurs <nathan.sancke@hotmail.fr>
2022-12-05 20:18:50 +0100

    Update du test Account pour utilise "useNavigate"

--- Web App Frontend/src/Pages/Account/Account.test.js
+++ Web App Frontend/src/Pages/Account/Account.test.js
@@ -1,13 +1,17 @@
 import {  render } from '@testing-library/react';
 import React from 'react';
 import Account from './Account';
+import {BrowserRouter} from "react-router-dom";
 
 /*Importing Config*/
 
 describe('Render Test for the Page Account', () => {
     it('Should render without crash', async () => {
         render(
-            <Account/>
+            <BrowserRouter>
+                <Account/>
+            </BrowserRouter>
+
         )
     })    
 })
