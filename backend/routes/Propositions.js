3ee2fea29c126f8f99dbd615d34efafa139f1823
KeviinKeurvels <k.keurvels@students.ephec-ti.be>
2022-12-06 19:08:03 +0100

    Page Detail Proposition
    Ajout de la page DetailProposition ainsi que des routes pour qu'elle ai
    toutes les informations dont elle a besoin
    
    Ajout du isActive dans le model car pull request la dessus pas encore
    passée

--- backend/routes/Propositions.js
+++ backend/routes/Propositions.js
@@ -1,48 +1,87 @@
 const express = require('express')
 const { Op } = require('sequelize')
 const router = express.Router()
-const {Propositions,Users,Pets} = require("../models")
+const { Propositions, Users, Pets } = require("../models")
 
 
 router.get("/", async (req, res) => {
 
     let offset = req.query.offset || 0
-    let limit = req.query.limit ? ((req.query.limit>0 && !isNaN(req.query.limit)) ? req.query.limit : 0): 10
-    let typeProposition = req.query.typeProposition || ["Promenade","Logement","Garde à domicile","Soins à domicile"]
-    let frequencyProposition = req.query.frequencyProposition || ["Régulière","Occasionnelle"]
-    let animalProposition = req.query.animalProposition || ["Chien","Chat","Rongeur","Oiseau","Poisson","NAC"]
+    let limit = req.query.limit ? ((req.query.limit > 0 && !isNaN(req.query.limit)) ? req.query.limit : 0) : 10
+    let typeProposition = req.query.typeProposition || ["Promenade", "Logement", "Garde à domicile", "Soins à domicile"]
+    let frequenceProposition = req.query.frequenceProposition || ["Régulière", "Occasionnelle"]
+    let animalProposition = req.query.animalProposition || ["Chien", "Chat", "Rongeur", "Oiseau", "Poisson", "NAC"]
 
-        const ListPropositions = await Propositions.findAll({
-            limit : parseInt(limit), 
-            offset: parseInt(offset),
-            where: {
-                Type : {[Op.or] : [typeProposition]},
-                Frequency : {[Op.or] : [frequencyProposition]},
-                Animal : {[Op.or] : [animalProposition]},
-                isActive:true
-            },
-            attributes : ['Type','Frequency','Animal'],
-                include : [{
-                    model : Users,
-                    attributes : ['FirstName', 'City', 'PhotoLink'],
-                    include : [{
-                        model: Pets,
-                        attributes : ['Type', 'Name'],
-                        }],
-                    }],
-        })
-        res.json(ListPropositions)
+    const ListPropositions = await Propositions.findAll({
+        limit: parseInt(limit),
+        offset: parseInt(offset),
+        where: {
+            Type: { [Op.or]: [typeProposition] },
+            Frequency: { [Op.or]: [frequenceProposition] },
+            Animal: { [Op.or]: [animalProposition] }
+        },
+        attributes: ['Type', 'Frequency', 'Animal'],
+        include: [{
+            model: Users,
+            attributes: ['FirstName', 'City', 'PhotoLink'],
+            include: [{
+                model: Pets,
+                attributes: ['Type', 'Name'],
+            }],
+        }],
+    })
+    res.json(ListPropositions)
 })
 
-router.post("/", async (req, res) => {
+router.get("/detailProposition", async (req, res) => {
+    const id = req.query.id <= 0 || isNaN(parseInt(req.query.id)) ? 1 : req.query.id
+
+    const detailOfProposition = await Propositions.findOne(
+        {
+            where: { id: id, isActive: true },
+            attributes: ['id', 'Type', 'Frequency', 'Animal', 'Number'],
+            include: [{
+                model: Users,
+                attributes: ['id', 'Firstname', 'Age', 'City', 'Postal']
+            }]
+        }
+    )
+    res.json(detailOfProposition)
+})
+
+router.put('/updateProposition', async (req, res) => {
     try {
-        const newProposition = await Propositions.create(req.body)
-        res.json(newProposition)
-    }
-    catch (error) {
-        res.status(400).send(error);
+        Propositions.update({
+            Type: req.body.Type,
+            Frequency: req.body.Frequency,
+            Animal: req.body.Animal,
+            Number: req.body.Number,
+        }, {
+            where: {
+                id: req.body.id
+            }
+        })
+
+        res.json(200)
+    } catch (error) { // en cas d'erreur
+        res.status(500).json(error);
     }
 })
 
+
+router.put("/deleteProposition", async (req, res) => {
+    try {
+        Propositions.update({
+            isActive: false
+        }, {
+            where: {
+                id: req.body.id
+            }
+        })
+        res.json(200);
+    } catch (error) { // en cas d'erreur
+        res.status(500).json(error);
+    }
+});
 
 module.exports = router
