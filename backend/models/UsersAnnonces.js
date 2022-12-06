2aee99a0827b0d7c841a54ff406a128d1bd0f261
TommyRiquet <tommy.riquet@hotmail.com>
2022-12-06 22:10:01 +0100

    Modification des modèles
    US #159
    
    Modifications des modèles pour permettre la liaison entre les annonces, les propositions et les users
    ça facilitera la gestion des requêtes dans les routes du backend

--- backend/models/UsersAnnonces.js
+++ backend/models/UsersAnnonces.js
@@ -2,5 +2,9 @@ module.exports = (sequelize) => {
 
     const UsersAnnonces = sequelize.define("UsersAnnonces", {})
 
+    UsersAnnonces.associate = (models) => {
+        UsersAnnonces.belongsTo(models.Users)
+    }
+
     return UsersAnnonces;
 }
--- backend/models/UsersPropositions.js
+++ backend/models/UsersPropositions.js
@@ -2,5 +2,9 @@ module.exports = (sequelize) => {
 
     const UsersPropositions = sequelize.define("UsersPropositions", {})
 
+    UsersPropositions.associate = (models) => {
+        UsersPropositions.belongsTo(models.Users)
+    }
+
     return UsersPropositions;
 }
