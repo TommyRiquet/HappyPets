24c7d424659f26cfaf5974d4c4c8342413b1952c
raphaelmarto <r.marto@students.ephec.be>
2022-12-08 11:02:17 +0100

    Ajout du Footer

--- Web App Frontend/src/Components/Footer/Footer.js
+++ Web App Frontend/src/Components/Footer/Footer.js
@@ -1,10 +1,17 @@
 import './Footer.css';
+import { Navbar, Container } from "react-bootstrap";
+import Nav from 'react-bootstrap/Nav';
 
 function Footer() {
     return (
-        <div className="Footer">
-
-        </div>
+      <Navbar expand="lg footer">
+        <Container fluid>
+          <Nav className="d-flex">
+            <Nav.Link className='link-orange' href={'/mentionslegales'}>{'Mentions légales'}</Nav.Link>
+            <Nav.Link className='link-orange' href={'mailto:happypets.support@hotmail.com'}>{'Contactez-Nous'}</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     );
 }
 
