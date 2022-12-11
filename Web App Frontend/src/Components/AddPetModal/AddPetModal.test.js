0ac48975e3c12bf5d7dc3fc9e87c3eccbc43735c
TommyRiquet <tommy.riquet@hotmail.com>
2022-12-11 12:42:31 +0100

    Uniformisation de composant AddPetModal
    J'ai retirer le composant AddPetModal car il va être utilisé dans
    d'autres pages que celles dans laquelle il a été crée

---
+++ Web App Frontend/src/Components/AddPetModal/AddPetModal.test.js
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react';
+import AddPetModal from './AddPetModal';
+
+describe('AddPetModal', () => {
+    it('Should render without crash', async () => {
+        
+    })
+})
