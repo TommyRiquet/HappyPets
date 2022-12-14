a82c853f19579041904e5a9f410d4856e50002e2
Lopidurs <nathan.sancke@hotmail.fr>
2022-12-14 14:48:57 +0100

    Plus de tests

--- Web App Frontend/src/Pages/CreatePet/CreatePet.test.js
+++ Web App Frontend/src/Pages/CreatePet/CreatePet.test.js
@@ -30,6 +30,10 @@ describe('CreatePet', () => {
             fireEvent.change(screen.getByTestId("sex"), {target: {value: 'M'}});
             fireEvent.change(screen.getByTestId("weight"), {target: {value: 2}});
             fireEvent.change(screen.getByTestId("height"), {target: {value: "Grand"}});
+            fireEvent.change(screen.getByTestId("sterile"), {target: {value: "Oui"}});
+            fireEvent.change(screen.getByTestId("dogFriendly"), {target: {value: "Oui"}});
+            fireEvent.change(screen.getByTestId("catFriendly"), {target: {value: "Oui"}});
+            fireEvent.change(screen.getByTestId("kidFriendly"), {target: {value: "Non"}});
             fireEvent.change(screen.getByTestId("comment"), {target: {value: "Super sympa"}});
         });
         await waitFor(() => {
@@ -40,6 +44,10 @@ describe('CreatePet', () => {
                 expect(screen.getByTestId("age").value).toBe("3");
                 expect(screen.getByTestId("weight").value).toBe("2");
                 expect(screen.getByTestId("height").value).toBe("Grand");
+                expect(screen.getByTestId("sterile").value).toBe("Oui");
+                expect(screen.getByTestId("dogFriendly").value).toBe("Oui");
+                expect(screen.getByTestId("catFriendly").value).toBe("Oui");
+                expect(screen.getByTestId("kidFriendly").value).toBe("Non");
                 expect(screen.getByTestId("comment").value).toBe("Super sympa");
             }
         );
@@ -51,14 +59,88 @@ describe('CreatePet', () => {
             </BrowserRouter>
         );
         await act(() => {
-            fireEvent.change(screen.getByTestId("name"), {target: {value: ''}});
-            fireEvent.change(screen.getByTestId("behaviour"), {target: {value: 'Dominant'}});
-            fireEvent.change(screen.getByTestId("type"), {target: {value: 'Chat'}});
             fireEvent.click(screen.getByTestId("submit"));
         })
         await waitFor(() => {
-            // expect(screen.getAllByText("Champ obligatoire").toBeInTheDocument())
             expect(screen.getAllByText("Champ obligatoire"))
+        })
+    })
+    it("Should display error message if field name is incorrect", async () => {
+        render(
+            <BrowserRouter>
+                <CreatePet/>
+            </BrowserRouter>
+        );
+        await act(() => {
+            fireEvent.change(screen.getByTestId("name"), {target: {value: 'S'}});
+            fireEvent.change(screen.getByTestId("behaviour"), {target: {value: 'Dominant'}});
+            fireEvent.change(screen.getByTestId("type"), {target: {value: 'Chat'}});
+            fireEvent.change(screen.getByTestId("race"), {target: {value: 'Ragdol'}});
+            fireEvent.change(screen.getByTestId("age"), {target: {value: 3}});
+            fireEvent.change(screen.getByTestId("sex"), {target: {value: 'M'}});
+            fireEvent.change(screen.getByTestId("weight"), {target: {value: 2}});
+            fireEvent.change(screen.getByTestId("height"), {target: {value: "Grand"}});
+            fireEvent.change(screen.getByTestId("sterile"), {target: {value: "Oui"}});
+            fireEvent.change(screen.getByTestId("dogFriendly"), {target: {value: "Oui"}});
+            fireEvent.change(screen.getByTestId("catFriendly"), {target: {value: "Oui"}});
+            fireEvent.change(screen.getByTestId("kidFriendly"), {target: {value: "Non"}});
+            fireEvent.change(screen.getByTestId("comment"), {target: {value: "Super sympa"}});
+            fireEvent.click(screen.getByTestId("submit"));
+        });
+        await waitFor(() => {
+            expect(screen.getAllByText("Doit faire 2 caractères ou plus"))
+        })
+    })
+    it("Should display error message if field Age is incorrect", async () => {
+        render(
+            <BrowserRouter>
+                <CreatePet/>
+            </BrowserRouter>
+        );
+        await act(() => {
+            fireEvent.change(screen.getByTestId("name"), {target: {value: 'test'}});
+            fireEvent.change(screen.getByTestId("behaviour"), {target: {value: 'Dominant'}});
+            fireEvent.change(screen.getByTestId("type"), {target: {value: 'Chat'}});
+            fireEvent.change(screen.getByTestId("race"), {target: {value: 'Ragdol'}});
+            fireEvent.change(screen.getByTestId("age"), {target: {value: -3}});
+            fireEvent.change(screen.getByTestId("sex"), {target: {value: 'M'}});
+            fireEvent.change(screen.getByTestId("weight"), {target: {value: 2}});
+            fireEvent.change(screen.getByTestId("height"), {target: {value: "Grand"}});
+            fireEvent.change(screen.getByTestId("sterile"), {target: {value: "Oui"}});
+            fireEvent.change(screen.getByTestId("dogFriendly"), {target: {value: "Oui"}});
+            fireEvent.change(screen.getByTestId("catFriendly"), {target: {value: "Oui"}});
+            fireEvent.change(screen.getByTestId("kidFriendly"), {target: {value: "Non"}});
+            fireEvent.change(screen.getByTestId("comment"), {target: {value: "Super sympa"}});
+            fireEvent.click(screen.getByTestId("submit"));
+        });
+        await waitFor(() => {
+            expect(screen.getAllByText("l'âge ne peut être négatif"))
+        })
+    })
+    it("Should display error message if field Poids is incorrect", async () => {
+        render(
+            <BrowserRouter>
+                <CreatePet/>
+            </BrowserRouter>
+        );
+        await act(() => {
+            fireEvent.change(screen.getByTestId("name"), {target: {value: 'test'}});
+            fireEvent.change(screen.getByTestId("behaviour"), {target: {value: 'Dominant'}});
+            fireEvent.change(screen.getByTestId("type"), {target: {value: 'Chat'}});
+            fireEvent.change(screen.getByTestId("race"), {target: {value: 'Ragdol'}});
+            fireEvent.change(screen.getByTestId("age"), {target: {value: 3}});
+            fireEvent.change(screen.getByTestId("sex"), {target: {value: 'M'}});
+            fireEvent.change(screen.getByTestId("weight"), {target: {value: 1000}});
+            fireEvent.change(screen.getByTestId("height"), {target: {value: "Grand"}});
+            fireEvent.change(screen.getByTestId("sterile"), {target: {value: "Oui"}});
+            fireEvent.change(screen.getByTestId("dogFriendly"), {target: {value: "Oui"}});
+            fireEvent.change(screen.getByTestId("catFriendly"), {target: {value: "Oui"}});
+            fireEvent.change(screen.getByTestId("kidFriendly"), {target: {value: "Non"}});
+            fireEvent.change(screen.getByTestId("comment"), {target: {value: "Super sympa"}});
+            fireEvent.click(screen.getByTestId("submit"));
+        });
+        await waitFor(() => {
+            expect(screen.getAllByText("le poids doit être compris entre 0 et 100 kg"))
         })
     })
 })
