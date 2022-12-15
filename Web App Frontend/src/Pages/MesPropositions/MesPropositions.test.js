f0242652ad7b520bf89788c880a8b1274860784a
quentinrld <71373028+quentinrld@users.noreply.github.com>
2022-12-15 10:51:19 +0100

    ajout d'un ";" 😢

--- Web App Frontend/src/Pages/MesPropositions/MesPropositions.test.js
+++ Web App Frontend/src/Pages/MesPropositions/MesPropositions.test.js
@@ -1,12 +1,17 @@
 import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom'
 import MesPropositions from './MesPropositions';
 
 describe('MesPropositions', () => {
     it('Should render without crash', async () => {
         localStorage.clear();
-        localStorage.setItem('user',JSON.stringify({id : 1}))
+        localStorage.setItem('user',JSON.stringify(
+                {id : 1}
+            ));
         render(
-            <MesPropositions/>
+            <BrowserRouter>
+                <MesPropositions/>
+            </BrowserRouter>
         )
     })
 })
