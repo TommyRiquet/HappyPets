a5fe27159320eeda7483b7b920f8045065a39d56
raphaelmarto <r.marto@students.ephec.be>
2022-12-21 18:09:52 +0100

    Modification Test User Adaptation du test user car les routes user on été modifier pour vérifier que seul le user ou l'admin puisse demander les information d'un user et ce test vérifie bien que n'importe qui ne peut pas récuperer les information d'un user

--- backend/test/Users.test.js
+++ backend/test/Users.test.js
@@ -3,10 +3,10 @@ const  app = require("../index");
 
 describe('Testing for the /users routes', function() {
 
-    it('Should responds 200 to /users/info', function (done) {
+    it('Should responds 500 to /users/info because no acces', function (done) {
         request(app)
             .get('/users/info?id=1')
-            .expect(200, done);
+            .expect(500, done);
     })  
   
   });
