21dda10e5591bb0c00919c7e0a647e2d6ffd2646
raphaelmarto <r.marto@students.ephec.be>
2022-12-21 17:36:48 +0100

    Test Pets Back end Ajout de test pour la route Pets back end pour renforcer la sécuriter et l'integriter de la route La route général pets , la route /pets/hasAnnonce et la route /pets/info on été tester dans le cas d'un bon fonctionnement et dans 2 cas ou elles ne sont pas sensé fonctionner

--- backend/test/Pets.test.js
+++ backend/test/Pets.test.js
@@ -11,3 +11,51 @@ describe('Testing for the /pets routes', function() {
   
   
   });
+
+  describe('Testing pets info', function() {
+
+    it('Should get the first pet', async() =>{
+        const res = await request(app)
+            .get('/pets/info/1')
+            expect(res.body[0].id).toBe(1);
+        
+    }) 
+
+    it('Should get an empty array cause no pet', async() =>{
+        const res = await request(app)
+            .get('/pets/info/-1')
+            expect(res.body).toEqual([]);
+        
+    }) 
+
+    it('Should get an empty array cause no pet ', async() =>{
+        const res = await request(app)
+            .get('/pets/info/a')
+            expect(res.body).toEqual([]);
+        
+    }) 
+});
+
+describe('Testing verify if it has anonnoncement', function() {
+
+    it('Should if pets 1  has an annonce linked', async() =>{
+        const res = await request(app)
+            .get('/pets/hasAnnonce?id=1')
+            expect(res.body).toBe(true);
+        
+    })
+    
+    it('Should get false because no pet', async() =>{
+        const res = await request(app)
+            .get('/pets/hasAnnonce?id=-1')
+            expect(res.body).toBe(false);
+        
+    })
+
+    it('Should get false because no pet', async() =>{
+        const res = await request(app)
+            .get('/pets/hasAnnonce?id=a')
+            expect(res.body).toBe(false);
+        
+    }) 
+});
